283a8701e989e9d1b0038b65cb239ae1fe26650d
Laurence <larfoley@yahoo.com>
2020-04-10 00:50:20 +0100

    refactor heating service

---
+++ services/heating/src/main/java/HeatingClient.java
@@ -0,0 +1,77 @@
+import heating.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HeatingClient {
+    private static final Logger logger = Logger.getLogger(HeatingClient.class.getName());
+    private final HeatingGrpc.HeatingBlockingStub blockingStub;
+    private final HeatingGrpc.HeatingStub asyncStub;
+    private ManagedChannel channel;
+
+    public HeatingClient() {
+        String target = "localhost:" + HeatingServer.PORT;
+
+        channel = ManagedChannelBuilder
+                .forTarget(target)
+                .usePlaintext()
+                .build();
+
+        blockingStub = HeatingGrpc.newBlockingStub(channel);
+        asyncStub = HeatingGrpc.newStub(channel);
+    }
+
+    public HeatingStatus checkStatus() {
+        HeatingStatus status;
+
+        try {
+            status = blockingStub.checkStatus(Empty.newBuilder().build());
+        }  catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return null;
+        }
+
+        return status;
+    }
+
+    public HeatingStatus turnOn () {
+        HeatingStatus status;
+
+        try {
+            status = blockingStub.turnOn(Empty.newBuilder().build());
+        }  catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return null;
+        }
+
+        return status;
+    }
+
+    public HeatingStatus turnOff () {
+        HeatingStatus status;
+
+        try {
+            status = blockingStub.turnOff(Empty.newBuilder().build());
+        }  catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return null;
+        }
+
+        return status;
+    }
+
+    public void adjustStrength (int strength) {
+        HeatingStrength heatingStrength = HeatingStrength.newBuilder()
+                .setStrength(strength)
+                .build();
+
+        try {
+            blockingStub.adjustStrength(heatingStrength);
+        }  catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+        }
+
+    }
+}
--- services/heating/src/main/java/HeatingServer.java
+++ services/heating/src/main/java/HeatingServer.java
@@ -9,37 +9,49 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 
-public class HeatingService {
-    private static final Logger logger = Logger.getLogger(HeatingService.class.getName());
+public class HeatingServer {
+    private static final Logger logger = Logger.getLogger(HeatingServer.class.getName());
 
     private Server server;
 
-    private void start() throws IOException {
-        int port = 50055;
+    public static final int PORT = 50055;
 
-        server = ServerBuilder.forPort(port)
+    private void start() throws IOException {
+
+        server = ServerBuilder.forPort(PORT)
                 .addService(new HeatingImpl())
                 .build()
                 .start();
 
-        // Create a JmDNS instance
-        JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+        registerJmDNS();
 
-        // Register a service
-        ServiceInfo serviceInfo = ServiceInfo.create("_http._tcp.local.", "example", 1234, "path=index.html");
-        jmdns.registerService(serviceInfo);
-
-        logger.info("Server started, listening on " + port);
+        logger.info("Server started, listening on " + PORT);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.err.println("* shutting down gRPC server since JVM is shutting down");
             try {
-                HeatingService.this.stop();
+                HeatingServer.this.stop();
             } catch (InterruptedException e) {
                 e.printStackTrace(System.err);
             }
             System.err.println("* server shut down");
         }));
+    }
+
+    private void registerJmDNS() {
+        try {
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+            ServiceInfo serviceInfo = ServiceInfo.create("_http._tcp.local.", "heating", 1237, "path=index.html");
+            jmdns.registerService(serviceInfo);
+
+            Thread.sleep(25000);
+
+            jmdns.unregisterAllServices();
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private void stop() throws InterruptedException {
@@ -55,7 +67,7 @@ public class HeatingService {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        final HeatingService server = new HeatingService();
+        final HeatingServer server = new HeatingServer();
         server.start();
         server.blockUntilShutdown();
     }
